d33ea250dc497f1593e334ee67d71982eee1fd73
nico <a.varticean@gmail.com>
2025-02-26 00:49:51 +0200

    Add models to match api

---
+++ Models/WeatherData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    class WeatherData
+    {
+        public float Temp { get; set; }
+        public Weather[] Weathers { get; set; }
+    }
+}
